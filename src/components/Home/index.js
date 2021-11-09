d934e9f65696816781058a5537ba224ae5768bd7
gopalch78 <gopal.ch78@gmail.com>
2021-11-09 15:38:40 +0000

    Functionality

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+
+const About = () => (
+  <>
+    <Header />
+    <div>
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+export default About
